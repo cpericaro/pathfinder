f2f201b3f6890d72ababe8ccf5e627cd1f3f230a
Carlos Periçaro <carloseduardopb2019@gmail.com>
2026-07-20 23:06:32 -0300

    feat: git init, and initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+Projeto de pathfinding, objetivo é fazer um algoritmo de pathfinging, que encontra a roda mais rapida num caminho com obstaculos
+
+Neste projeto sera utilizado:
+C, Javascript
+
---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+int main()
+{
+    //definindo as struct's "Square" e "Map"
+    struct Square
+    {
+        int x, y;
+    };
+
+    //mapa de 50x50
+    struct Map
+    {
+        int x = 50;
+        int y = 50;
+    };
+
+    for (int i = 0; i < map.x; i++)
+    {
+        for (int j = 0; j < map.y; j++)
+        {
+            std::cout << ".";
+        }
+        std::cout << "\n";
+    }
+
+    //criando instancias do tipo Square
+    Square start;
+    Square end;
+
+    //recebendo os valores para as instancias 'start' e 'end'
+    std::cout << "Enter the coordinates of the starting point\n (x): ";
+    std::cin >> start.x;
+    std::cout << " (y): ";
+    std::cin >> start.y;
+
+    std::cout << "Enter the coordinates of the ending point\n (x): ";
+    std::cin >> end.x;
+    std::cout << " (y): ";
+    std::cin >> end.y;
+
+    //validando impressão dos valores recebidos
+	std::cout << "\n\nStart values:\n X: " << start.x << ", Y: " << start.y;
+    std::cout << ";\n End values:\n X: " << end.x << ", Y: " << end.y << ";";
+	return 0;
+} 
